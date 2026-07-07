d279d8f40bb62c3ea166ddb46121560d616a0930
BastardCC <ainamandarine@gmail.com>
2026-07-07 14:51:31 +0300

    feature: Init services section

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Header from "./_components/Header";
 import Hero from "./_components/Hero";
 import Projects from "./_components/Projects";
+import Services from "./_components/Services";
 import { TransitionProvider } from "./_components/ProjectsAwardsTransition";
 
 export default function Home() {
@@ -11,8 +12,11 @@ export default function Home() {
         <section className="relative z-0">
           <Hero />
         </section>
-        <section className="relative z-10 my-30">
+        <section className="relative z-10 mt-30">
           <Projects />
+        </section>
+        <section className="relative z-10 bg-[#0a0909] text-background">
+          <Services />
         </section>
       </div>
     </TransitionProvider>
